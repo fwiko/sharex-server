75b2fcaf060ddd6038bca26bb8643d19c9e976f3
Raffrerty <raffsimms23@gmail.com>
2022-05-04 13:41:56 +0100

    Changed File Size Limit Method

--- src/app.js
+++ src/app.js
@@ -3,6 +3,12 @@ const path = require('path');
 const express = require('express');
 const fileUpload = require('express-fileupload');
 const config = require('../data/config');
+const utility = require('./utility');
+
+// timestamps for console.log messages
+require('console-stamp')(console, {
+    format: ':date(yyyy/mm/dd HH:MM:ss) :label'
+});
 
 // initialise environment variables from .env file
 require('dotenv').config({ path: '.env' });
@@ -11,7 +17,12 @@ require('dotenv').config({ path: '.env' });
 const server = express();
 
 // initialise file upload middleware
-server.use(fileUpload());
+server.use(fileUpload({
+    limits: {
+        fileSize: config.files.maxSize * 1024 * 1024,
+        files: 1
+    },
+}));
 
 // trust X-Forwarded-* headers
 server.set('trust proxy', config.server.proxied);
